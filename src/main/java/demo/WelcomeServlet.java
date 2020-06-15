88c0c0b4f35a1eace3f7cf1fe5f4b76b5c31f093
Ionut <ionut.lazar300@gmail.com>
2020-06-15 17:59:56 +0300

    Initial commit

---
+++ src/main/java/demo/WelcomeServlet.java
@@ -0,0 +1,27 @@
+package demo;
+
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class WelcomeServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 2376899699873237398L;
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+		
+		final Enumeration<String> names = request.getParameterNames();
+		
+		while (names.hasMoreElements()) {
+			final String name = names.nextElement();
+			final String value = request.getParameter(name);
+			
+			System.out.println(name + " = " + value);
+		}
+		
+	}
+
+}
